0b16903b8001ef7e71ccff412e582c8b797a3e3c
Kunal Kaklij <kkaklij@walterpmoore.com>
2024-05-17 21:46:00 +0530

    first commit

---
+++ src/test/java/Automation/Automation/AppTest.java
@@ -0,0 +1,31 @@
+package Automation.Automation;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+	WebDriver driver;
+    @Test
+    public void setUp()
+    {
+    	System.out.println("Hello Guys");
+    	WebDriverManager.chromedriver().setup();
+    	driver = new ChromeDriver();
+    	driver.get("https://kunalazurecourse.azurewebsites.net/webapp/");
+    	String text =driver.findElement(By.cssSelector("h1")).getText();
+		System.out.println(text);
+		Assert.assertTrue(text.equalsIgnoreCase("RahulShettyAcademy.com Learning"));
+		driver.quit();    }
+}
